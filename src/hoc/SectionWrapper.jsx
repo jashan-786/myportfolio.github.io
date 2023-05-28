27860cc649ecd51dcdda03208c8b8425b6a7ab12
Jashan <jashanpreet28122001@gmail.com>
2023-05-27 21:32:22 -0500

    project completed

---
+++ src/hoc/SectionWrapper.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+import { motion } from "framer-motion";
+import { staggerContainer } from "../utils/motion";
+
+import { styles } from "../styles";
+
+const SectionWrapper = (Component, idName) =>
+  function HOC() {
+    return (
+      <motion.section
+        variants={staggerContainer()}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: true, amount: 0.25 }}
+        className={`${styles.padding} max-w-7xl  mx-auto realtive z-0`}
+      >
+        <span className="hash-span" id={idName}></span>
+        <Component />
+      </motion.section>
+    );
+  };
+
+export default SectionWrapper;
